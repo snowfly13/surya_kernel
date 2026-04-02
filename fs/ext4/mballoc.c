b19d67a4530b4bb2490d6ccf4700abb2468f89e8
Zqiang <qiang.zhang@linux.dev>
2026-04-02 13:40:37 -0400

    ext4: fix the might_sleep() warnings in kvfree()
    [ Upstream commit 496bb99b7e66f48b178126626f47e9ba79e2d0fa ]
    
    Use the kvfree() in the RCU read critical section can trigger
    the following warnings:
    
    EXT4-fs (vdb): unmounting filesystem cd983e5b-3c83-4f5a-a136-17b00eb9d018.
    
    WARNING: suspicious RCU usage
    
    ./include/linux/rcupdate.h:409 Illegal context switch in RCU read-side critical section!
    
    other info that might help us debug this:
    
    rcu_scheduler_active = 2, debug_locks = 1
    
    Call Trace:
     <TASK>
     dump_stack_lvl+0xbb/0xd0
     dump_stack+0x14/0x20
     lockdep_rcu_suspicious+0x15a/0x1b0
     __might_resched+0x375/0x4d0
     ? put_object.part.0+0x2c/0x50
     __might_sleep+0x108/0x160
     vfree+0x58/0x910
     ? ext4_group_desc_free+0x27/0x270
     kvfree+0x23/0x40
     ext4_group_desc_free+0x111/0x270
     ext4_put_super+0x3c8/0xd40
     generic_shutdown_super+0x14c/0x4a0
     ? __pfx_shrinker_free+0x10/0x10
     kill_block_super+0x40/0x90
     ext4_kill_sb+0x6d/0xb0
     deactivate_locked_super+0xb4/0x180
     deactivate_super+0x7e/0xa0
     cleanup_mnt+0x296/0x3e0
     __cleanup_mnt+0x16/0x20
     task_work_run+0x157/0x250
     ? __pfx_task_work_run+0x10/0x10
     ? exit_to_user_mode_loop+0x6a/0x550
     exit_to_user_mode_loop+0x102/0x550
     do_syscall_64+0x44a/0x500
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
     </TASK>
    
    BUG: sleeping function called from invalid context at mm/vmalloc.c:3441
    in_atomic(): 1, irqs_disabled(): 0, non_block: 0, pid: 556, name: umount
    preempt_count: 1, expected: 0
    CPU: 3 UID: 0 PID: 556 Comm: umount
    Call Trace:
     <TASK>
     dump_stack_lvl+0xbb/0xd0
     dump_stack+0x14/0x20
     __might_resched+0x275/0x4d0
     ? put_object.part.0+0x2c/0x50
     __might_sleep+0x108/0x160
     vfree+0x58/0x910
     ? ext4_group_desc_free+0x27/0x270
     kvfree+0x23/0x40
     ext4_group_desc_free+0x111/0x270
     ext4_put_super+0x3c8/0xd40
     generic_shutdown_super+0x14c/0x4a0
     ? __pfx_shrinker_free+0x10/0x10
     kill_block_super+0x40/0x90
     ext4_kill_sb+0x6d/0xb0
     deactivate_locked_super+0xb4/0x180
     deactivate_super+0x7e/0xa0
     cleanup_mnt+0x296/0x3e0
     __cleanup_mnt+0x16/0x20
     task_work_run+0x157/0x250
     ? __pfx_task_work_run+0x10/0x10
     ? exit_to_user_mode_loop+0x6a/0x550
     exit_to_user_mode_loop+0x102/0x550
     do_syscall_64+0x44a/0x500
     entry_SYSCALL_64_after_hwframe+0x77/0x7f
    
    The above scenarios occur in initialization failures and teardown
    paths, there are no parallel operations on the resources released
    by kvfree(), this commit therefore remove rcu_read_lock/unlock() and
    use rcu_access_pointer() instead of rcu_dereference() operations.
    
    Fixes: 7c990728b99e ("ext4: fix potential race between s_flex_groups online resizing and access")
    Fixes: df3da4ea5a0f ("ext4: fix potential race between s_group_info online resizing and access")
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Reviewed-by: Baokun Li <libaokun@linux.alibaba.com>
    Link: https://patch.msgid.link/20260319094545.19291-1-qiang.zhang@linux.dev
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@kernel.org
    [ adapted inline rcu_read_lock/rcu_dereference/rcu_read_unlock removal ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -2582,9 +2582,7 @@ err_freebuddy:
 	rcu_read_unlock();
 	iput(sbi->s_buddy_cache);
 err_freesgi:
-	rcu_read_lock();
-	kvfree(rcu_dereference(sbi->s_group_info));
-	rcu_read_unlock();
+	kvfree(rcu_access_pointer(sbi->s_group_info));
 	return -ENOMEM;
 }
 
@@ -2778,7 +2776,8 @@ int ext4_mb_release(struct super_block *sb)
 	struct ext4_sb_info *sbi = EXT4_SB(sb);
 	struct kmem_cache *cachep = get_groupinfo_cache(sb->s_blocksize_bits);
 
-	if (sbi->s_group_info) {
+	group_info = rcu_access_pointer(sbi->s_group_info);
+	if (group_info) {
 		for (i = 0; i < ngroups; i++) {
 			cond_resched();
 			grinfo = ext4_get_group_info(sb, i);
@@ -2793,12 +2792,9 @@ int ext4_mb_release(struct super_block *sb)
 		num_meta_group_infos = (ngroups +
 				EXT4_DESC_PER_BLOCK(sb) - 1) >>
 			EXT4_DESC_PER_BLOCK_BITS(sb);
-		rcu_read_lock();
-		group_info = rcu_dereference(sbi->s_group_info);
 		for (i = 0; i < num_meta_group_infos; i++)
 			kfree(group_info[i]);
 		kvfree(group_info);
-		rcu_read_unlock();
 	}
 	kfree(sbi->s_mb_offsets);
 	kfree(sbi->s_mb_maxs);
