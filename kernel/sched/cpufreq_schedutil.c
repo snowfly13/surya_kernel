5a012eea9410d0c5979a763967f278e1236b7eaa
Debiandra <deby1317@gmail.com>
2026-04-23 02:27:41 +0700

    Adjust CPU frequency scaling

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -749,7 +749,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->rate_limit_us = 2000;
+	tunables->rate_limit_us = 1500;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
